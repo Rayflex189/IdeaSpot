41d3063bc57133da5de4a0a6717816c5ccc52baa
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-05-22 17:58:17 +0100

    Update serializers.py
    Save

--- backend/apps/branches/serializers.py
+++ backend/apps/branches/serializers.py
@@ -1,1 +1,13 @@
+from rest_framework import serializers
+from .models import EditedBranch, BranchHistory
 
+class EditedBranchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EditedBranch
+        fields = '__all__'
+        read_only_fields = ['id', 'version', 'created_at', 'updated_at']
+
+class BranchHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BranchHistory
+        fields = '__all__'
