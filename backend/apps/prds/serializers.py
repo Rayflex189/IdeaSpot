628ecca138237e2ef95c61db00c4308b1479e8bd
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-05-22 17:49:20 +0100

    Update serializers.py
    Save

--- backend/apps/prds/serializers.py
+++ backend/apps/prds/serializers.py
@@ -1,1 +1,31 @@
+from rest_framework import serializers
+from .models import PRD, PRDPermission
 
+class PRDSerializer(serializers.ModelSerializer):
+    owner = serializers.SerializerMethodField()
+    has_edit_permission = serializers.SerializerMethodField()
+
+    class Meta:
+        model = PRD
+        fields = '__all__'
+        read_only_fields = ['id', 'fingerprint', 'is_protected']
+
+    def get_owner(self, obj):
+        return {
+            'id': obj.idea.created_by.id,
+            'username': obj.idea.created_by.username
+        }
+
+    def get_has_edit_permission(self, obj):
+        request = self.context.get('request')
+        if not request or not request.user.is_authenticated:
+            return False
+        if obj.idea.created_by == request.user:
+            return True
+        return PRDPermission.objects.filter(prd=obj, user=request.user, can_edit=True).exists()
+
+class PRDPermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PRDPermission
+        fields = '__all__'
+        read_only_fields = ['id', 'granted_by', 'created_at']
