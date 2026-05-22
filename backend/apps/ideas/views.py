3881702e5c98d059786fd90baa6ab794554258e5
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-05-22 17:41:10 +0100

    Update views.py
    Save

--- backend/apps/ideas/views.py
+++ backend/apps/ideas/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Idea
+from .serializers import IdeaSerializer
 
-# Create your views here.
+class IdeaListCreateView(generics.ListCreateAPIView):
+    serializer_class = IdeaSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Idea.objects.filter(created_by=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+class IdeaDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = IdeaSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Idea.objects.filter(created_by=self.request.user)
