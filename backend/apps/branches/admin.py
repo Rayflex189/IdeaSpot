97c7afd3f732aed6a46768429666dc731557a56a
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-05-22 18:04:44 +0100

    Update admin.py
    Save

--- backend/apps/branches/admin.py
+++ backend/apps/branches/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import EditedBranch, BranchHistory
 
-# Register your models here.
+@admin.register(EditedBranch)
+class EditedBranchAdmin(admin.ModelAdmin):
+    list_display = ('prd', 'last_edited_by', 'version', 'updated_at')
+
+@admin.register(BranchHistory)
+class BranchHistoryAdmin(admin.ModelAdmin):
+    list_display = ('branch', 'edited_by', 'version', 'created_at')
