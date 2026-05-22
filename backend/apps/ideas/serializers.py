16b15b2dd9997b91981c335057a41893ddf6b521
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-05-22 17:39:41 +0100

    Update serializers.py
    Save

--- backend/apps/ideas/serializers.py
+++ backend/apps/ideas/serializers.py
@@ -1,1 +1,11 @@
 
+from rest_framework import serializers
+from .models import Idea
+
+class IdeaSerializer(serializers.ModelSerializer):
+    created_by_username = serializers.ReadOnlyField(source='created_by.username')
+
+    class Meta:
+        model = Idea
+        fields = '__all__'
+        read_only_fields = ['id', 'created_by', 'created_at', 'updated_at', 'status']
